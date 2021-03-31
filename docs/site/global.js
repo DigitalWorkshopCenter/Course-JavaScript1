696f03325f600b039fe8c30b6ca29b1e0ff4d336
Nathan Birch <birchn@byui.edu>
2021-03-31 10:00:33 -0600

    adding images

---
+++ docs/site/global.js
@@ -0,0 +1,37 @@
+// global constants
+var g = {};
+
+// course info
+g.courseCode = '';
+g.courseName = 'Intro to JavaScript';
+g.courseCodeName = g.courseCode + ' | ' + g.courseName;
+
+// module numbers
+g.n01 = '01';
+g.n02 = '02';
+g.n03 = '03';
+g.n04 = '04';
+g.n05 = '05';
+g.n06 = '06';
+g.n07 = '07';
+g.n08 = '08';
+g.n09 = '09';
+g.n10 = '10';
+g.n11 = '11';
+g.n12 = '12';
+g.n13 = '13';
+
+// module names
+g.u01 = 'Lesson ' + g.n01;
+g.u02 = 'Lesson ' + g.n02;
+g.u03 = 'Lesson ' + g.n03;
+g.u04 = 'Lesson ' + g.n04;
+g.u05 = 'Lesson ' + g.n05;
+g.u06 = 'Lesson ' + g.n06;
+g.u07 = 'Lesson ' + g.n07;
+g.u08 = 'Lesson ' + g.n08;
+g.u09 = 'Lesson ' + g.n09;
+g.u10 = 'Lesson ' + g.n10;
+g.u11 = 'Lesson ' + g.n11;
+g.u12 = 'Lesson ' + g.n12;
+g.u13 = 'Lesson ' + g.n13;
