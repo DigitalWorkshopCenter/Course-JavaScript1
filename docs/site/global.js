7dc4e0141ae7f77f1f4a4ca9b2e9dbb3d5eab30a
Nathan Birch <birchn@byui.edu>
2021-04-01 12:00:56 -0600

    setting up 5 lesson outcomes

--- docs/site/global.js
+++ docs/site/global.js
@@ -12,26 +12,15 @@ g.n02 = '02';
 g.n03 = '03';
 g.n04 = '04';
 g.n05 = '05';
-g.n06 = '06';
-g.n07 = '07';
-g.n08 = '08';
-g.n09 = '09';
-g.n10 = '10';
-g.n11 = '11';
-g.n12 = '12';
-g.n13 = '13';
 
 // module names
-g.u01 = 'Lesson ' + g.n01;
-g.u02 = 'Lesson ' + g.n02;
-g.u03 = 'Lesson ' + g.n03;
-g.u04 = 'Lesson ' + g.n04;
-g.u05 = 'Lesson ' + g.n05;
-g.u06 = 'Lesson ' + g.n06;
-g.u07 = 'Lesson ' + g.n07;
-g.u08 = 'Lesson ' + g.n08;
-g.u09 = 'Lesson ' + g.n09;
-g.u10 = 'Lesson ' + g.n10;
-g.u11 = 'Lesson ' + g.n11;
-g.u12 = 'Lesson ' + g.n12;
-g.u13 = 'Lesson ' + g.n13;
+g.u01 = 'Lesson ' + g.n01 + ' - Introduction to JavaScript';
+g.u02 = 'Lesson ' + g.n02 + ' - Data Structures';
+g.u03 = 'Lesson ' + g.n03 + ' - Document Object Model';
+g.u04 = 'Lesson ' + g.n04 + ' - Web Application Part I';
+g.u05 = 'Lesson ' + g.n05 + ' - Web Application Part II';
+
+// assignment information
+g.reading = "Reading Materials";
+g.classTime = "In-class Materials";
+g.assignment = "Programming Assignment";
