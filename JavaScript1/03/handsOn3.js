e37138a388a4c513c49017d43356d8cb61d7d3d1
Nathan Birch <birchn@byui.edu>
2021-06-15 14:17:44 -0600

    changing double quotes to single quotes in week three class activity

--- JavaScript1/03/handsOn3.js
+++ JavaScript1/03/handsOn3.js
@@ -1,9 +1,9 @@
 function addAttendee() {
-    let name = birthdayForm.first.value + " " + birthdayForm.last.value;
-    
-    let ul = document.getElementById("attendees");
+  let name = birthdayForm.first.value + ' ' + birthdayForm.last.value;
 
-    let li = document.createElement("LI");
-    li.appendChild(document.createTextNode(name));
-    ul.appendChild(li);
+  let ul = document.getElementById('attendees');
+
+  let li = document.createElement('LI');
+  li.innerHTML = name;
+  ul.appendChild(li);
 }
